3d5a6e806fd69230f80e39e1104785328f0db009
Oumaimaelkaoun <oumi.elkaoun@gmail.com>
2023-03-22 03:47:48 -0700

    task 04

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,31 +10,4 @@ int _isalpha(int c)
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	return (0);
-}
-------------------------------------------
-#include <stdio.h>
-#include "main.h"
-
-/**
- * print_sign - print_sign
- * @n : variable integer
- * Return: 0 (SUCCESS)
- */
-int print_sign(int n)
-{
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
-}
+}}
