4943c4658a0fcd1a3be9182ab78b0ae707c76faf
Oumaimaelkaoun <oumi.elkaoun@gmail.com>
2023-04-13 14:43:59 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - check malloc
+ * @b: u i
+ * Return: pointer to allocate
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void	*str;
+
+	str = malloc(b);
+	if (!str)
+		exit(98);
+	return (str);
+}
